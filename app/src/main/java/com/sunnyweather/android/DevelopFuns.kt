d899345c069ba92e824912ac7e476092ffc86a68
Karry <496110920@qq.com>
2021-03-04 17:07:09 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/DevelopFuns.kt
@@ -0,0 +1,11 @@
+package com.sunnyweather.android
+
+import android.widget.Toast
+
+fun String.showToast(duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(SunnyWeatherApplication.context, this, duration).show()
+}
+
+fun Int.showToast(duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(SunnyWeatherApplication.context, this, duration).show()
+}
